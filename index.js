8feaf7c4b0c7079736bfbe023fb9aeb48142fadc
developer-soc-sp <developer.soc.sp@gmail.com>
2019-01-22 10:53:48 +0800

    test

--- index.js
+++ index.js
@@ -2,6 +2,38 @@ var express = require("express");
 var app = express();
 
 
+app.get("/personality-test",function(req,res){
+
+    var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
+    var personality_insights = new PersonalityInsightsV3({
+        iam_apikey: 'FfhAfiZqgW4aUhn-ftKdLIFy70GDn0lo6k7PyQkUkfrO',
+        version_date: '2017-10-13'
+    });
+
+    var params = {
+    content: `In Moulmein, in Lower Burma, I was hated by large numbers of people — the only time in my life that I have been important enough for this to happen to me. I was sub-divisional police officer of the town, and in an aimless, petty kind of way anti-European feeling was very bitter...`,
+    content_type: 'text/plain',
+    consumption_preferences: true,
+    raw_scores: true
+    };
+
+    personality_insights.profile(params, function(error, response) {
+        if (error)
+            console.log('Error:', error);
+        else
+            console.log(JSON.stringify(response, null, 2));
+            // Send the HTTP header 
+            // HTTP Status: 200 : OK
+            // Content Type: text/plain
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            //let imgs = JSON.parse(results);
+            // Send the response body as "Hello World"
+            res.end('Hello World\n' + result);
+        }
+    );
+
+})
+
 app.get("/image-test", function (req, res) {
 
     var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
@@ -29,7 +61,7 @@ app.get("/image-test", function (req, res) {
             res.writeHead(200, { 'Content-Type': 'text/plain' });
             //let imgs = JSON.parse(results);
             // Send the response body as "Hello World"
-            res.end('Hello World\n' + response.images.constructor.name + result);
+            res.end('Hello World\n' + response.images.constructor.name + "\n" + result);
         }
     });
 
